583df208f0d6e6e4a9f2fd63e8664d9f8855ec74
Joncarlos Perdomo <joncarlosperdomo@gmail.com>
2022-03-13 22:24:00 -0400

    Updated Login/Sign in button

--- components/MenuAppBar.js/index.js
+++ components/MenuAppBar.js/index.js
@@ -1,14 +1,18 @@
 import * as React from "react";
 import { Box, Button } from "@mui/material";
-
+import Link from "next/link";
 export default function MenuAppBar() {
   const [auth, setAuth] = React.useState(true);
   const [anchorEl, setAnchorEl] = React.useState(null);
 
   return (
     <Box sx={{ maxWidth: "100vw", textAlign: "right", m: 2 }}>
-      <Button variant="contained">Login</Button>
-      <Button>Sign Up</Button>
+      <Link href="/login" passHref>
+        <Button variant="contained">Login</Button>
+      </Link>
+      <Link href="/login" passHref>
+        <Button>Sign Up</Button>
+      </Link>
     </Box>
   );
 }
